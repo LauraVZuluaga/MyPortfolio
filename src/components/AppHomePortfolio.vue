c82892b510d103f7ae882f05991454cca90d626e
LauraVZuluaga <49324952+LauraVZuluaga@users.noreply.github.com>
2021-12-27 23:11:58 -0500

    Home section finish

---
+++ src/components/AppHomePortfolio.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <section class="home-section aling-items-center">
+      <div class="container">
+        <div class="row aling-items-center">
+          <div class="home-text">
+            <p>Hello, I'm</p>
+            <h1>Laura Zuluaga</h1>
+            <h2>Frontend Developer</h2>
+            <a href="#" class="btn">More about me</a>
+            <a href="#" class="btn">Portfolio</a>
+          </div>
+          <div class="home-img">
+            <div class="img-box">
+              <img src="@/assets/programmer.png" alt="programmer-img" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app-home-portfolio",
+};
+</script>
+
+<style lang="scss"></style>
